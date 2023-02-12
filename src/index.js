2f633bfc05bfeb1f41d8a0017c7081f449f4d908
sqaya <124000807+sqaya@users.noreply.github.com>
2023-02-11 22:45:24 -0800

    function

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ function search(event) {
 function displayCelsiusTemp(event){
 event.preventDefault();
   let celsiusTemp = (14 − 32) * 5/9;
-  alert("hello");
+  alert(celsiusTemp);
 }
 
 let searchForm = document.querySelector("#search-form");
