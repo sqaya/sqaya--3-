694f728b3dacdbe38ee0eb4d7e463e7fdddd8b8a
sqaya <124000807+sqaya@users.noreply.github.com>
2023-02-11 21:36:59 -0800

    icon adapted

--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ function displayWeatherCondition(response) {
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
 let iconElement = document.querySelector("#icon");
-iconElement.setAttribute("src", `https://openweathermap.org/img/wn/04d@2x.png`);
+iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 function search(event) {
   event.preventDefault();
