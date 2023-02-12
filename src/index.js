df404a107660030222a4b405099cb4f2ad647215
sqaya <124000807+sqaya@users.noreply.github.com>
2023-02-11 22:52:12 -0800

    link clicked

--- src/index.js
+++ src/index.js
@@ -52,8 +52,8 @@ function search(event) {
 
 function displayCelsiusTemp(event){
 event.preventDefault();
-  let celsiusTemp = (14 − 32) * 5/9;
-  alert(celsiusTemp);
+  
+  alert("link clicked");
 }
 
 let searchForm = document.querySelector("#search-form");
