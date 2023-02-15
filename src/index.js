2aa3986efe801a43e5c73c5f56dc9c132724e4b3
sqaya <saronayala@yahoo.com>
2023-02-14 23:22:11 -0800

    forecast function improved in JS

--- src/index.js
+++ src/index.js
@@ -15,10 +15,31 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let day = days[dayIndex];
   return `${day} ${hours}:${minutes}`;
+}
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  let dias = ["Thu", "Fri", "Sat", "Sun"];
+  dias.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+<div class="col-2">
+   <div class="weather-forecast-date">${day}</div>  
+   <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="" width="42">
+<div class="weather-forecast-temps"> <span class="temp-max">18°</span> <span class="temp-min">12°</span> 
+</div> 
+</div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
@@ -27,21 +48,22 @@ dateElement.innerHTML = formatDate(currentTime);
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  
+
   fTemperature = response.data.main.temp;
-    document.querySelector("#temp").innerHTML = Math.round(fTemperature);
-  
+  document.querySelector("#temp").innerHTML = Math.round(fTemperature);
+
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind-speed").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-  
-   
-    
-let iconElement = document.querySelector("#icon");
-iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function search(event) {
   event.preventDefault();
@@ -54,18 +76,20 @@ function search(event) {
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function displayCelsiusTemp(event){
-event.preventDefault();
-let celsiusTemperature = (fTemperature-32)*5/9;
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  let celsiusTemperature = ((fTemperature - 32) * 5) / 9;
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 function displayFahrenheitTemp(event) {
-event.preventDefault();
-let temperatureElement = document.querySelector("#temp");
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(fTemperature);
 }
 let fTemperature = null;
+
+displayForecast();
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
